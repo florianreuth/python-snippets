b4906354a16d2b2ffa3751dcc8957915ebf188d0
Flori2007 <60033407+florianmichael@users.noreply.github.com>
2020-12-28 21:10:51 +0100

    Add Tubeln Examples

---
+++ datastructures/tupeln.py
@@ -0,0 +1,23 @@
+#Programmed by Florian Michael
+#since 28.12.2020
+#
+#Examples to use Tupelns in Python
+#The values in a tuple are not changeable
+#and the declaration happens differently, 
+#otherwise they do not differ from lists or dictionaries
+#Declaration:
+firstTupeln = "<3",
+nextTupeln = "Hello", "World", "I", "Like", "Python"
+
+nextTupeln = nextTupeln + firstTupeln;
+
+#Getting:
+print (nextTupeln)
+
+#Other:
+#You can make a certain value in a tuple changeable by adding
+#it to a list and saving the list to the tuple:
+lastTupeln = "Hello", "You", "are", [15]
+print (lastTupeln)
+lastTupeln[3][0] = 10;
+print (lastTupeln)
