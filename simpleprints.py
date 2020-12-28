2e3e7a1dd2e244dc9c7c2f5fde7188f9f2f13d67
Flori2007 <60033407+florianmichael@users.noreply.github.com>
2020-12-28 13:25:07 +0100

    Add Print Examples

---
+++ simpleprints.py
@@ -0,0 +1,8 @@
+#Programmed by Florian Michael
+#since 28.12.2020
+#
+#In Python you can use either " or ' in the print method to indicate a
+#string, if you want to use " in a string you can for example mark the 
+#string itself with '.
+print("Welcome to the Python-Examples")
+print('This is a nice "Repository"')
