9d88b5c0888795789518bfc27e8aa907a821ac93
Flori2007 <60033407+florianmichael@users.noreply.github.com>
2020-12-28 13:34:37 +0100

    Add MathTests and Prints for Manage

---
+++ task1/mathtests.py
@@ -0,0 +1,10 @@
+#Programmed by Florian Michael
+#since 28.12.2020
+#
+#This print method prints the text "5+2" on the screen, 
+#this is because the text is in encoding characters.
+print ("5+2")
+#This print method prints the text "7" on the screen,
+#this is because the text is not in encoding characters and the 
+#Python interpreter calculates the task and then outputs it.
+print (5 + 2)
---
+++ task1/trader.py
@@ -0,0 +1,9 @@
+#Programmed by Florian Michael
+#since 28.12.2020
+#
+#German
+print ("Verwaltung zu Bestanden")
+print ("")
+print ("Mit diesem Programm können sie ihre Einnahmen verwalten")
+#TODO:
+# - Add System for Manage "Einnahmen"
