f50617bda47e07a428a69106aecb9bcefaca6d02
Flori2007 <60033407+florianmichael@users.noreply.github.com>
2020-12-29 14:59:29 +0100

    Add Simple ElseIf Examples

---
+++ conditionfunctions/elseif.py
@@ -0,0 +1,18 @@
+#Programmed by Florian Michael
+#since 29.12.2020
+#
+#Examples for IF with ELSE KeyWord in Python
+first = input()
+if first == "Hello":
+    print('First is "Hello"')
+else:
+    print('First is not "Hello"')
+    
+print("Next Step")
+
+if first == "Hello":
+    print('First is "Hello"')
+elif first == "Test":
+    print('First is "Test"')
+else:
+    print("Fuck You!")
