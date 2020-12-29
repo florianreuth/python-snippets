42365e9b74587c57a5c1a63c91b46bdf34f5cb5d
Flori2007 <60033407+florianmichael@users.noreply.github.com>
2020-12-29 14:48:39 +0100

    Add Simple If Example and ConditionFunctions Folder

---
+++ conditionfunctions/simpleif.py
@@ -0,0 +1,36 @@
+#Programmed by Florian Michael
+#since 28.12.2020
+#
+#Examples for IF KeyWord in Python
+a = "Test"
+if a == "Test":
+    print("Meddl") 
+
+print("Next Step")    
+
+b = 5
+c = eval(input())
+if b == 5 and c == 4:
+    print("Ok!")
+    
+print("Next Step")   
+ 
+if not c > 5:
+    print("C is not bigger than 5")
+
+print("Next Step")    
+
+if not (c > 5 and b == 5):
+    print("This is ez")
+
+print("Next Step")    
+a = "Test"
+b = "Exploit"
+
+if a == "Test" or b == "Meddl":
+    print("Ok Now it works")
+    
+print("Next Step")    
+
+if not (a == "Test" or b == "Meddl"):
+    print("Ok Now it works222")
