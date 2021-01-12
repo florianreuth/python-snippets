d6b88587cd97ec5471a6259ebc902fa5cc3986b4
FlorianMichael <60033407+florianmichael@users.noreply.github.com>
2021-01-12 15:24:50 +0100

    Beginn with Classes (Yay :D)

---
+++ ObjectRevolution/classes/classes.py
@@ -0,0 +1,13 @@
+class Test:
+    """
+    This is a Example Class for Object Programming in Python 3
+    """
+    def __init__(self, name, age):
+        """
+        These are some Test Vars
+        """
+        self.Name = name
+        self.Age = age
+
+testInstance = Test("Florian", 13)
+       
