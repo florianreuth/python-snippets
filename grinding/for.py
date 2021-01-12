85fd054ebe7bb2848c27393901f59ab21fd6fa86
FlorianMichael <60033407+florianmichael@users.noreply.github.com>
2021-01-12 10:18:29 +0100

    Improve for.py

--- grinding/for.py
+++ grinding/for.py
@@ -6,3 +6,10 @@ a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 for aEntry in a:
     print ("Value ", aEntry)
+
+#If you want to Access to Fields in Lists:
+b = [5, 8, 10, 4]
+print ("Before Edit ", b)
+for bEntry in enumerate(b):
+    b[bEntry[0]] = bEntry * 2
+print ("After Edit ", b)
