db7a8cb01adc1970b1d12c4406031ad55ba80ccf
FlorianMichael <60033407+florianmichael@users.noreply.github.com>
2021-01-12 15:38:24 +0100

    Improve Classes

--- ObjectRevolution/classes/classes.py
+++ ObjectRevolution/classes/classes.py
@@ -2,12 +2,28 @@ class Test:
     """
     This is a Example Class for Object Programming in Python 3
     """
-    def __init__(self, name, age):
+
+    #Static Vars before __init__ (Java: Constructor)
+    countOfTest = 0
+
+    def __init__(self, name, age, __hobby):
+        Test.countOfTest += 1
         """
         These are some Test Vars
         """
         self.Name = name
         self.Age = age
+        self.__Hobby = __hobby #It is not possible to edit this and you can not access this like (Java: Protected & Final)
+                                                # <-
+    def getHobby(self): #Bypass for Access 
+        return self.__Hobby    
 
-testInstance = Test("Florian", 13)
-       
+testInstance = Test("Florian", 13, "Programming")
+print (testInstance.Name)
+print (testInstance.getHobby()) #Access Var
+
+print (Test.countOfTest) #Access Var
+
+"""
+Def in Classes are Methods, Def out of Classes are Functions
+"""
