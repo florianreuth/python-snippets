9a9d8abe8eb2d5991374501a0e99b123daaece45
FlorianMichael <60033407+florianmichael@users.noreply.github.com>
2021-01-12 10:39:29 +0100

    Random Shit

---
+++ task5/snpwhilefor.py
@@ -0,0 +1,15 @@
+#Programmed by Florian Michael
+#since 29.12.2020
+#
+#Random shit
+#
+#For
+for listEntry in range(10):
+    print ("Value: ", listEntry + 1)
+#
+print ("Next Step")
+#While
+count = 0
+while (count < 10):
+    count += 1
+    print ("Value: ", count)
