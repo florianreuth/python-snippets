8959ce35549474c27c8758101d5f171282381724
FlorianMichael <60033407+florianmichael@users.noreply.github.com>
2021-01-12 11:22:02 +0100

    Add New Example for modules

--- ObjectRevolution/modules/tests/main.py
+++ ObjectRevolution/modules/tests/main.py
@@ -1,5 +1,9 @@
 import test_module
 import folder.folder_module
 
+simpleModule = folder.folder_module # You dont need to import the Module before
+
 print (test_module.getShit())
 print (folder.folder_module.getShit())
+print ("Next Step")
+print (simpleModule.getShit())
