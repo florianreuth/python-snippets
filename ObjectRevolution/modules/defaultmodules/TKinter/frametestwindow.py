04e5f6cb60f7bb65703e3edfd1b56d28bf11e2ad
FlorianMichael <60033407+florianmichael@users.noreply.github.com>
2021-01-15 20:24:26 +0100

    Add Window with Nice Border

--- ObjectRevolution/modules/defaultmodules/TKinter/frametestwindow.py
+++ ObjectRevolution/modules/defaultmodules/TKinter/frametestwindow.py
@@ -6,7 +6,10 @@ window.geometry("400x200") #Set Window Size
 
 window.title("First TKinter Program") #Set Window Title
 
-welcomeText = Label(text = "Welcome to TKinter!") #Create a Text
-welcomeText.pack() #Add Text to Window
+testFrame = Frame(window, relief = "ridge", borderwidth=20) #Create Border with width 20 and ridge Design
+testFrame.pack(fill="both", expand=1) #Expand the Border to the whole Screen
+
+welcomeText = Label(testFrame, text = "Welcome to TKinter!") #Create a Text (Add Border)
+welcomeText.pack(expand = 1) #Add Text to Window (Expand = 1 ^= Width = width / 2, Height = height / 2 ^= Center)
 
 window.mainloop() #Loop the Window and Render it
